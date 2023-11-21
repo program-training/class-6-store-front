cad65e0e122f56b28207d31245c4bda607e8b48d
yonathan wiezel <yswiezel@gmail.com>
2023-11-21 15:57:58 +0200

    layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,19 +1,28 @@
-import { Outlet } from "react-router-dom"
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const Layout = () => {
   return (
     <>
-     <Header/>
-     <div>
-        <div></div>
-        <Outlet/>
-     </div>
-     <Footer/>
+      <Header />
+      <div style={{
+        display: 'flex', justifyContent: "center",
+        backgroundColor: "whitesmoke"
+      }}>
+        <div
+          style={{
+            minHeight: "83vh",
+            backgroundColor: "purple",
+            width: "92%",
+          }}
+        >
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
