09bce7b79b8f32f3f6c74102f200446475ceae7b
doodi pincus <d0533114605@gmail.com>
2023-11-26 14:40:22 +0200

    for avishy

--- src/rtk/userNameSlice.ts
+++ src/rtk/userNameSlice.ts
@@ -33,11 +33,11 @@ export const userNameSlice = createSlice({
       state.lastName = action.payload.lastName ? action.payload.lastName : null;
       state.email = action.payload.email ? action.payload.email : null
     },
-    resetUserName: (state) => {
-      state = initialState;
-    },
+    // resetUserName: (state) => {
+    //   state = initialState;
+    // },
   },
 });
 
-export const { setUserName, resetUserName } = userNameSlice.actions;
+export const { setUserName } = userNameSlice.actions;
 export default userNameSlice.reducer;
